218e9220fd124d019717b0176017ead9824249fd
Edcarlos <edcarlos.almeida@hotmail.com>
2021-03-02 14:42:24 -0300

    New ExplorerPath Class

---
+++ ExplorerPath.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDRAW_SwTools{
+
+  class ExplorerPath{
+
+    const string sourcePath = @"C:\Users\Projeto01\Desktop\EDRAW SwTools\EDRAW-SW-TOOLS\WinPath";        
+    const string targetPath = @"P:\TESTES EDDY\EDDY\";
+    const string currentPath = "Pattern";
+    const string makePath = "221-0000-TESTE FILE EXPLORER";
+
+    static void Main(){            
+      
+      if (Directory.Exists(targetPath+"\\"+makePath)){
+          Console.WriteLine("Directory exist");
+      }else{
+
+        var oldPath = sourcePath+"\\"+currentPath;
+        var newPath = sourcePath+"\\"+makePath;
+
+        Directory.Move(oldPath,newPath);
+
+        //make Folder
+        foreach (var path in Directory.GetDirectories(sourcePath, "*",SearchOption.AllDirectories)){
+            Directory.CreateDirectory(path.Replace(sourcePath,targetPath));
+        }
+        // Copy file
+        foreach (var iFile in Directory.GetFiles(sourcePath,"*",SearchOption.AllDirectories)){
+            File.Copy(iFile , iFile.Replace(sourcePath,targetPath),true);
+        }
+        Directory.Move(newPath,oldPath);
+      }  
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -7,16 +7,17 @@ using System.Windows.Forms;
 namespace EDRAW_SwTools
 {
     static class Program
-    {
+    {/*
         /// <summary>
         /// Ponto de entrada principal para o aplicativo.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            
+        }*/
     }
 }
